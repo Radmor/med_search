462e1d5aeba2f54dea847553b5c8d3825c9897c7
sefirq <sebastian.firlik@student.put.poznan.pl>
2017-05-30 17:59:57 +0200

    Add Jaccard Similarity measure

--- src/citations/utils.py
+++ src/citations/utils.py
@@ -3,7 +3,7 @@ import nltk
 from nltk.corpus import stopwords
 
 DESCRIBING_METHODS = {'tf': 'term_frequency', 'tfidf': 'term_frequency_inverse_document_frequency'}
-COMPARISON_METHODS = {'cos': 'cosine_similarity', 'euc': 'euclidean_distance_similarity'}
+COMPARISON_METHODS = {'cos': 'cosine_similarity', 'euc': 'euclidean_distance_similarity', 'jac': 'jaccard_similarity'}
 
 
 def length_of_vector(list_of_values):
@@ -21,6 +21,14 @@ def euclidean_distance_similarity(terms_in_query, terms_in_result):
     sqrt from sum of squares of differences of coordinates
     """
     return math.sqrt(sum([(terms_in_result[term] - terms_in_query[term])**2 for term in terms_in_result.keys()]))
+
+
+def jaccard_similarity(terms_in_query, terms_in_result):
+    words_in_both_query_and_result = 0
+    for term in terms_in_result.keys():
+        if terms_in_result[term] > 0 and terms_in_query[term] > 0:
+            words_in_both_query_and_result += 1
+    return words_in_both_query_and_result*1.0/len(terms_in_query.keys())
 
 
 def measure_similarity(documents, query, result, content_describing_method, comparison_method):
@@ -45,14 +53,15 @@ def measure_similarity(documents, query, result, content_describing_method, comp
 def term_frequency(documents, query, result_as_a_list):
     terms_in_query = {}
     terms_in_result = {}
+    for term in query + result_as_a_list:
+        terms_in_query[term] = terms_in_query.get(term, 0)
+        terms_in_result[term] = terms_in_result.get(term, 0)
     for term in query:
-        terms_in_query[term] = terms_in_query.get(term, 0) + 1
+        terms_in_query[term] += 1
     for term in terms_in_query.keys():
         terms_in_query[term] /= len(query)
-        terms_in_result[term] = terms_in_result.get(term, 0)
-        for word in result_as_a_list:
-            if term == word:
-                terms_in_result[word] += 1
+    for word in result_as_a_list:
+        terms_in_result[word] += 1
     for word in terms_in_result.keys():
         terms_in_result[word] /= len(result_as_a_list)
     return terms_in_query, terms_in_result
@@ -82,11 +91,13 @@ def parse_query(query):
 
 
 if __name__ == '__main__':
+    print(term_frequency([], ['aa', 'ab', 'ac', 'aa', 'ad'],['aa', 'ba', 'bb', 'ac']))
+    print(jaccard_similarity({'aa': 0.4, 'ab': 0.2, 'ad': 0.2, 'ba': 0.0, 'bb': 0.0, 'ac': 0.2}, {'aa': 0.25, 'ab': 0.0, 'ad': 0.0, 'ba': 0.25, 'bb': 0.25, 'ac': 0.25}))
     # There is a working example of tfidf
-    print(term_frequency_inverse_document_frequency([['aa', 'ab', 'ac'], ['aa', 'ab', 'aa'], ['aa', 'ad', 'ab', 'ac', 'ad'], ['ab','ab','ab']], ['aa', 'ab', 'ac'], {'title': 'aa ad ab ac ad'}))
-    print(1/3*math.log(5/3), 1/3*math.log(5/4), 1/3*math.log(5/2))
-    print(0.2 * math.log(5 / 3), 0.2 * math.log(5 / 4), 0.2 * math.log(5 / 2))
+    # print(term_frequency_inverse_document_frequency([['aa', 'ab', 'ac'], ['aa', 'ab', 'aa'], ['aa', 'ad', 'ab', 'ac', 'ad'], ['ab','ab','ab']], ['aa', 'ab', 'ac'], {'title': 'aa ad ab ac ad'}))
+    # print(1/3*math.log(5/3), 1/3*math.log(5/4), 1/3*math.log(5/2))
+    # print(0.2 * math.log(5 / 3), 0.2 * math.log(5 / 4), 0.2 * math.log(5 / 2))
     # print(measure_similarity([], ['cancer', 'symptom'], {"title": "cancer lol lol lol lol lol symptom"}, DESCRIBING_METHODS["tf"], COMPARISON_METHODS['cos']))
     # print(([], ['cancer', 'symptom'], {"title": "cancer lol lol lol lol lol"}, "cos"))
     # print(([], ["cancer", "cancer", "boom", "trick"], {"title": "cancer boom lol cool boom thing"}, "cos"))
-    print(euclidean_distance_similarity({'a': 0.0, 'b': 0.0}, {'a': 1.0, 'b': 1.0}))
+    # print(euclidean_distance_similarity({'a': 0.0, 'b': 0.0}, {'a': 1.0, 'b': 1.0}))
